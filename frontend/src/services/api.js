4223ebf9f0d67c5f74507506851dda84538d67f2
Shreya Maurya <you@example.com>
2026-05-19 11:16:55 +0530

    Update URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ese-backend-vt0c.onrender.com",
+  baseURL: "https://ese-backend-vt0c.onrender.com/api",
 });
 
 export default API;
